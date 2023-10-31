b700a382b6ba4d9b0566303b7e1b2c7345bb83fe
Danny van der Kraan <dvanderkraan@xpirit.com>
2023-10-31 16:52:30 +0100

    Add project files.

---
+++ Performance.ArrayPool/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Buffers;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+public class ArrayPoolBenchmark
+{
+    private ArrayPool<int> pool = ArrayPool<int>.Shared;
+
+    [Benchmark]
+    public void ProcessWithoutArrayPool()
+    {
+        int[] array = new int[10000];
+        // ... process the array ...
+    }
+
+    [Benchmark]
+    public void ProcessWithArrayPool()
+    {
+        int[] rentedArray = pool.Rent(10000);
+        // ... process the array ...
+        pool.Return(rentedArray);
+    }
+
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<ArrayPoolBenchmark>();
+    }
+}
